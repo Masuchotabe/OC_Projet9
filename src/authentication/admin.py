6767cc74e304ea9886ee3214472151b550558bf5
Masuchotabe <moreauthomas0@gmail.com>
2023-10-18 21:03:34 +0200

    Enregistrement des modèles dans l'admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from authentication.models import User
+from reviews.models import Review, Ticket, UserFollows, UserBlocked
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Review)
+admin.site.register(Ticket)
+admin.site.register(UserFollows)
+admin.site.register(UserBlocked)
