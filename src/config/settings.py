96e8a4cfca820502f5821e17d4117c222091d0e8
Masuchotabe <moreauthomas0@gmail.com>
2023-10-25 19:58:03 +0200

    config media settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # URL LOGIN
 
 LOGIN_REDIRECT_URL = '/home/'
